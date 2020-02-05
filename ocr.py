d02481b775538b7818a879c9f0f83ebca912bb69
MKrsna <krsna@localhost.localdomain>
2020-02-05 17:56:56 +0530

    Offline mode complete

--- ocr.py
+++ ocr.py
@@ -3,6 +3,7 @@ from PIL import Image, ImageChops
 import pytesseract
 import numpy as np
 import argparse
+import shutil
 import cv2
 import collections
 import os
@@ -13,21 +14,21 @@ multiplier = 2
 class Record:
     def __init__(self, name, image, date):
         self.keys = [name]
-        self.image = image
+        self.images = [image]
         self.date = date.split("to")
-        cv2.rectangle(self.image, (0, 0),
-                      (120, self.image.shape[0]), (255, 255, 255), -1)
-        self.image = cv2.putText(self.image, str(self.date[0]).upper().strip(), (10, 20),
-                                 cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 0), 2)
-        self.image = cv2.putText(self.image, "TO", (50, 50),
-                                 cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 0), 2)
-        self.image = cv2.putText(self.image, str(self.date[1]).upper().strip(), (10, 80),
-                                 cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 0), 2)
+        self.prepareImage()
 
-        # cv2.imshow("test", self.image)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
-        # exit(0)
+    def prepareImage(self):
+        for image in self.images:
+            # cv2.imshow('a', image)
+            cv2.rectangle(image, (0, 0),
+                          (120, image.shape[0]), (255, 255, 255), -1)
+            image = cv2.putText(image, str(self.date[0]).upper().strip(), (10, 20),
+                                cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 0), 2)
+            image = cv2.putText(image, "TO", (50, 50),
+                                cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 0), 2)
+            image = cv2.putText(image, str(self.date[1]).upper().strip(), (10, 80),
+                                cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 0), 2)
 
     def addKey(self, key):
         self.keys.append(key)
@@ -36,15 +37,30 @@ class Record:
         return (key in self.keys)
 
     def getImage(self):
-        return self.image
+        self.prepareImage()
+        return self.images[0]
 
     def __str__(self):
         return "-".join(self.keys)
 
 
-class RecordsHolder:
+class RecordCollection:
     def __init__(self):
+        self.records = list()
+
+    def addRecord(self, rcd: Record):
+        # if self.findRecordByName(rcd.name)
+        # for name in rcd.name:
+        # if not self.findRecordByName(rcd.keys):
+        self.records.append(rcd)
         pass
+
+    def findRecordByName(self, names: []) -> Record:
+        for rcd in self.records:
+            for name in names:
+                if rcd.hasKey(name):
+                    return rcd
+        print("Record Not found")
 
 
 def getImageText(img) -> str:
@@ -69,6 +85,9 @@ def getRecordImages(img):
 
 
 def getNameBlock(img):
+    # cv2.imshow('s', img[0:60, 100:500])
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
     return img[0:60, 100:500]
 
 
@@ -76,49 +95,49 @@ def getDateBlock(img):
     return img[0:45, :]
 
 
-def getRecordsDict(pdfPath):
-    rcd = []
-    images = convert_from_path(pdfPath, dpi=200)
-    for x in range(len(images)):
-        images[x].save("tmpimg/out{}.jpg".format(x), 'JPEG')
-        img = cv2.imread("tmpimg/out{}.jpg".format(x))
-        date = getImageText(getDateBlock(img)).replace(
+def getRecordsCollection(pdfPath, rcd) -> RecordCollection:
+    pages = convert_from_path(pdfPath, dpi=200)
+    for x in range(len(pages)):
+        pages[x].save("tmpimg/out{}.jpg".format(x), 'JPEG')
+        page = cv2.imread("tmpimg/out{}.jpg".format(x))
+        date = getImageText(getDateBlock(page)).replace(
             "salary sheet", "").replace("()", "").strip().replace(", ", " 20")
-        records = getRecordImages(img)
+        records = getRecordImages(page)
         for s in records:
             names = getImageText(getNameBlock(s)).splitlines()
-            tmpRecord = Record(names[0], s, date)
+            tmpRecord = Record(" ".join(names), s, date)
             if len(names) > 1:
                 [tmpRecord.addKey(names[x]) for x in range(1, len(names))]
-            rcd.append(tmpRecord)
+            rcd.addRecord(tmpRecord)
     return rcd
 
 
-def findRecord(records: [], name):
-    for rcd in records:
-        if rcd.hasKey(name):
-            return rcd
-    print("Record Not found")
+def createDirs():
+    names = ['tmpimg', 'records', 'tmp']
+    for name in names:
+        shutil.rmtree('./{}/'.format(name))
+        os.makedirs("./{}/".format(name))
 
 
-files = os.listdir("./pdfs/")
-dcts = []
+pdfs = os.listdir("./pdfs/")
+createDirs()
+
+dcts = RecordCollection()
 
 print("processing")
-for file in files:
-    print(file)
-    dcts.append(getRecordsDict('pdfs/{}'.format(file)))
+
+for pdf in pdfs:
+    print(pdf)
+    getRecordsCollection('pdfs/{}'.format(pdf), dcts)
 
 namesList = collections.defaultdict(list)
-print("processed")
 
-for dctx in dcts:
-    for dct in dctx:
-        for name in dct.keys:
-            namesList[name].append(dct.getImage())
+for record in dcts.records:
+    namesList[record.keys[0]].append(record.getImage())
 
 for k in namesList.keys():
-    cv2.imwrite("records/{}.jpg".format(k), cv2.vconcat(namesList[k]))
+    cv2.imwrite("records/{}.jpg".format(str(k).replace("/", "-")),
+                cv2.vconcat(namesList[k]))
 
-
+print("processed")
 # cv2.destroyAllWindows()
